518d6ce3af8e4934f3c8675fb980e4121d7c447e
Grinmak <grinmak@gmail.com>
2022-07-13 12:13:17 +0300

    add keyboard event listener, fix bug then one operator goes after another

--- script2.js
+++ script2.js
@@ -4,6 +4,8 @@ let numberToCalculateFurther;;
 let operatorSymbol;
 let numberToDisplay = '';
 const storeData = {};
+const displayLimit = 9;
+let countDigits = 0;
 
 
 const result = document.querySelector('.result');
@@ -13,8 +15,44 @@ allButtons.forEach(btn => btn.addEventListener('click', getValue));
 const twoSpecialButton = document.getElementById('spec_btn').querySelectorAll('button');
 twoSpecialButton.forEach(btn => btn.addEventListener('click', getValueSpec));
 
-const displayLimit = 10;
-let countDigits = 0;
+window.addEventListener('keydown', keyBoardInput);
+
+function keyBoardInput(event) {
+  console.log(event.key);
+  if(event.key === 'Delete') {chooseOperator('C')};
+  if(event.key === 'Backspace'){chooseOperator('DEL')}
+  if(event.key === '.' && numberToDisplay.length > 0 && !storeData.comma){
+     storeData.comma = true;
+     numberToDisplay += '.';
+     return result.textContent = `${numberToDisplay}`;
+  }
+   if(Number(event.key) || event.key === '0'){
+      while(countDigits < displayLimit){
+         numberToDisplay += event.key;
+         console.log(numberToDisplay);
+         countDigits ++;
+         return result.textContent = `${numberToDisplay}`;
+      }; 
+   };
+   if (event.key === '/' || event.key === '*'||
+       event.key === '-' || event.key === '+'){ 
+         if(storeData.operatorSymbol && numberToDisplay.length > 0){
+             storeData.secondNumber = Number(numberToDisplay);
+             chooseOperator(storeData.operatorSymbol)
+         } 
+       delete storeData.comma;
+       if(numberToDisplay.length > 0){
+          storeData.firstNumber = Number(numberToDisplay); 
+         }
+       countDigits = 0;
+       storeData.operatorSymbol = event.key;
+       numberToDisplay = '';   
+   }else if (event.key === 'Enter'){
+      storeData.secondNumber = Number(numberToDisplay);
+      chooseOperator(storeData.operatorSymbol);
+   }
+};
+
 
 function getValue (event){
    if(event.target.classList.contains('comma') &&
@@ -30,82 +68,41 @@ function getValue (event){
          return result.textContent = `${numberToDisplay}`;
         }
     }if (event.target.classList.contains('operator')){ 
-        if(storeData.operatorSymbol){
+        if(storeData.operatorSymbol  && numberToDisplay.length > 0){
             storeData.secondNumber = Number(numberToDisplay);
             chooseOperator(storeData.operatorSymbol)
         } 
       delete storeData.comma;
-      storeData.firstNumber = Number(numberToDisplay); 
+        if(numberToDisplay.length > 0){
+            storeData.firstNumber = Number(numberToDisplay); 
+         }
       countDigits = 0;
-      storeData.operatorSymbol = this.textContent;
+      storeData.operatorSymbol = this.textContent; // не убирать глубже в if
       numberToDisplay = '';   
     }else if (event.target.classList.contains ('operator_get_result')){
-      storeData.secondNumber = numberToDisplay;
+      storeData.secondNumber = Number(numberToDisplay);
       chooseOperator(storeData.operatorSymbol);
    }
 }
 
-/* function chooseOperator(symbol){
-   switch(symbol) {
-      case '/':
-         divide();
-         break;
-      case 'x':
-         multiply();
-         break;
-      case '-':
-         subtract();
-         break;
-      case '+':
-         sum();
-         break;
-      case 'C':
-         clearAllInputs();
-         break;
-      case 'DEL':
-         deleteLastDigit();
-         break;
-   }
-   
-} */
 function chooseOperator(symbol){
-   if(symbol === '/') {numberToDisplay = storeData.firstNumber / storeData.secondNumber};
-   if(symbol === 'x') {numberToDisplay = storeData.firstNumber * storeData.secondNumber};
-   if(symbol === '-') {numberToDisplay = storeData.firstNumber - storeData.secondNumber};
-   if(symbol === '+') {numberToDisplay = storeData.firstNumber + storeData.secondNumber};
-   if(symbol === 'C') {clearAllInputs()};
-   if(symbol === 'DEL') {deleteLastDigit()};
-   resetOperator();
+   if(symbol !== 'DEL'){
+      if(symbol === '/') {numberToDisplay = storeData.firstNumber / storeData.secondNumber};
+      if(symbol === 'x' || symbol === '*') {numberToDisplay = storeData.firstNumber * storeData.secondNumber};
+      if(symbol === '-') {numberToDisplay = storeData.firstNumber - storeData.secondNumber};
+      if(symbol === '+') {numberToDisplay = storeData.firstNumber + storeData.secondNumber};
+      if(symbol == 'C') {clearAllInputs()};
+      resetOperator();
+   }else if(symbol == 'DEL') {deleteLastDigit()};
+   // numberToDisplay = parseFloat(numberToDisplay).toFixed(7);
+   // let numToString = String(numberToDisplay).slice(0, 10);
+   // numberToDisplay = Number(numToString);
    return result.textContent = `${numberToDisplay}`;                
 }
 
 function resetOperator(){
    delete storeData.operatorSymbol;
 }
-/*
-function divide(){
-  numberToDisplay = storeData.firstNumber / storeData.secondNumber;
-  resetOperator();
-  return result.textContent = `${numberToDisplay}`;
-};
-
-function multiply(){
-    numberToDisplay = storeData.firstNumber * storeData.secondNumber;
-    resetOperator();
-    return result.textContent = `${numberToDisplay}`;
-};
-
-function subtract(){
-    numberToDisplay = storeData.firstNumber - storeData.secondNumber;
-    resetOperator();
-    return result.textContent = `${numberToDisplay}`;
-};
-
-function sum(){
-    numberToDisplay = Number(storeData.firstNumber) + Number(storeData.secondNumber);
-    resetOperator();
-    return result.textContent = `${numberToDisplay}`;
-}; */
 
 function clearAllInputs(){
    countDigits = 0;
@@ -123,6 +120,7 @@ function deleteLastDigit(){
    getAnArray.pop();
    countDigits--;
    numberToDisplay = Number(getAnArray.join(''));
+   storeData.secondNumber = Number(numberToDisplay);
    // numberToDisplay = Number(getAnArray);
    return result.textContent = `${numberToDisplay}`;
 }
